365dce44523bcf48c97027532c577349a6eba176
JOKWIAE <gkdiqkf@naver.com>
2022-07-01 14:48:20 +0900

    2022-06-23 연습문제5

---
+++ Test/05/5_08.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ *날짜: 2022/06/23 
+ *이름: 조귀애
+ *내용: 컬렉션 연습문제
+ */
+
+namespace Test._05
+{
+    internal class _5_08
+    {
+    }
+}
