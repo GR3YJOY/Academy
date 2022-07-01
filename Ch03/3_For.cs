5b24d3e71f018debf3ba62b3c706e6bd7d116efd
JOKWIAE <gkdiqkf@naver.com>
2022-07-01 14:28:23 +0900

    2022-06-07 수업예제

---
+++ Ch03/3_For.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 날짜: 2022/06/07
+ * 이름: 조귀애
+ * 내용: 반복문 실습하기 교재 p157,166
+ *       반복문(Loop)은 프로그래밍에서 동일한 명령을 일정횟수만큼 반복해서 수행하도록 제어하는 명령문
+ *       for(초기식; 조건식; 증감식)
+ */
+
+namespace Ch03
+{
+    internal class _3_For
+    {
+        static void Main1(string[] args)
+        {
+            //for문
+            for (int i = 1; i <= 5; i++)  //초기식:변수선언, 조건식과 증감식이 참으로 성립, 조건식의 숫자에 따라 반복수도 달라짐
+            {
+                Console.WriteLine("{0}회 반복...", i);
+            }
+
+
+            //1부터 10까지 합
+            int sum = 0;
+
+            for (int k = 1; k<= 10; k++)
+            {
+                sum += k; // sum = sum + k;
+            }
+
+            Console.WriteLine("1부터 10까지 합 : " + sum);
+
+
+            //1부터 10까지 짝수합
+            int tot = 0;
+
+            for (int v = 1; v <= 10; v++)
+            {
+                if (v % 2 == 0) // 짝수라면 : 나머지가 0, 홀수라면 : 나머지가 1
+                {
+                    tot += v;
+                }
+            }
+
+            Console.WriteLine("1부터 10까지 짝수합 :" +tot);
+
+            //중첩 for문
+            for (int a=1; a<=3; a++) //3번 반복
+            {
+                Console.WriteLine("a : " + a);
+                for (int b=1; b<=4; b++) //4번 반복
+                {
+                    Console.WriteLine("b :" + b);
+                    for (int c = 1; c <= 5; c++) 
+                    {
+                        Console.WriteLine("c :" + c); //5번 반복 : 총 3번*4번*5번 = 60번 반복
+                    }
+                }
+            }
+
+            //구구단
+            for (int x = 2; x <= 9; x++) 
+            {
+                Console.WriteLine("{0}단 시작", x);
+                for (int y = 1; y <=9; y++)
+                {
+                    int z = x * y;
+
+                    Console.WriteLine("{0} x {1} = {2}", x, y, z);
+                }
+            }
+
+
+            //별삼각형
+            for (int start = 1; start <= 10; start++)
+            {
+                for (int end = 1; end <= start; end++)
+                {
+                    Console.Write("★");
+                }
+                Console.WriteLine(); // 줄바꿈
+            }
+
+            Console.WriteLine(); // 줄바꿈
+
+
+            //역별삼각형
+            for (int start1 = 10; start1 >= 1; start1--)
+            {
+                for (int end1 = 1; end1 <= start1; end1++)
+                {
+                    Console.Write("★");
+                }
+                Console.WriteLine();
+                
+            }
+        }
+             
+    }
+}
---
+++ Ch03/4_While.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 날짜 : 2022/06/07
+ * 이름 : 조귀애
+ * 내용 : 반복문 while 실습하기 교재 p162 
+ *        for문은 반복횟수가 정해져있지만, while문은 그렇지않으며 조건에 따라 반복
+ */
+
+namespace Ch03
+{
+    internal class _4_While
+    {
+        static void Main1(string[] args)
+        {
+            // 1부터 10까지 합
+            int sum = 0;
+            int k = 1;             //초기식
+            while (k <= 10)        //while (조건식)
+            {
+                sum += k;
+                k++;               //증감식
+            }
+
+            Console.WriteLine("1부터 10까지 합 : " +sum);
+
+
+            // do ~ while : 최초 한번은 무조건 반복 실행, 그 이후 반복할 지 정하는 것
+            int total = 0;
+            int i = 1;
+
+            do
+            {
+                if (i % 2 == 0)
+                {
+                    total += i;
+                }
+                i++;                 //탈출을 위해 변수 더함
+            }
+            while(i <= 10);
+
+            Console.WriteLine("1부터 10까지 짝수합 : "+total);
+
+
+            // break
+            int num = 0;
+            
+            while (true)
+            {
+                if (num % 5 == 0 && num % 7 == 0)
+                {
+                    break;              //반복문 종료
+                }
+                num++;
+            }
+
+            Console.WriteLine("5와 7의 최소공배수 : "+num);
+
+
+            // continue
+            int tot = 0;
+            int j = 0;
+
+            while (j <= 10)
+            {
+                if (j % 2 == 1)
+                {
+                    continue;         //반복문의 처음으로 이동
+                }
+
+                tot += j;
+            }
+
+            Console.WriteLine("1부터 10까지 짝수합 : " +tot);
+
+        }
+    }
+}
