0f7d8fb6a094cd21dab975a5b565da9c9dcd20db
JOKWIAE <gkdiqkf@naver.com>
2022-07-01 14:46:13 +0900

    2022-06-10 연습문제2

---
+++ Test/02/2_10.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//피보나치 수열 연습문제
+
+namespace Test._01
+{
+    internal class _2_10
+    {
+        static void Main10(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(fibo(i) + " ");
+            }
+        }
+
+        public static int fibo(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
+}
