1b8bf62be9d59521f0aa979f177ed38881b4b48b
JOKWIAE <gkdiqkf@naver.com>
2022-07-01 14:47:01 +0900

    2022-06-16(결석) 연습문제3

---
+++ Test/03/3_04.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * 날짜 : 2022/06/16 (결석)
+ * 이름 : 조귀애
+ * 내용 : 클래스 변수 연습문제
+ * 
+ * static 변수 == 정적변수 == 클래스 변수
+ */
+
+namespace Test._03
+{
+    internal class _3_04
+    {
+    }
+}
