eb74f0bd24f28a38d3f5db17d341bbaa50fa5f90
JOKWIAE <gkdiqkf@naver.com>
2022-07-01 14:21:56 +0900

    2022-06-10 백준 코딩테스트

---
+++ Baekjoon/01단계/Main1_10.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon._01단계
+{
+    internal class Main1_10
+    {
+        static void Main1(string[] args)
+        {
+            string id = Console.ReadLine();
+
+            Console.WriteLine(id+ "??!"); 
+        }
+    }
+}
---
+++ Baekjoon/02단계/Main2_01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon._02단계
+{
+    internal class Main2_01
+    {
+        static void Main1(string[] args)
+        {
+            string n = Console.ReadLine();
+            string[] nn = n.Split();
+
+            int a, b;
+            a = int.Parse(nn[0]);
+            b = int.Parse(nn[1]);
+
+            if (a > b)
+            {
+                Console.WriteLine(">");
+            }
+
+            else if (a < b)
+            {
+                Console.WriteLine("<");
+            }
+
+            else 
+            {
+                Console.WriteLine("==");
+            }
+        }
+    }
+}
