18b64d74a8c47c7373e17849474b59fe7fe9014d
JOKWIAE <gkdiqkf@naver.com>
2022-07-01 14:48:36 +0900

    2022-06-24 연습문제6

---
+++ Test/06/6_09.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜: 2022/06/24
+ * 이름: 조귀애
+ * 내용: 데이터베이스 프로그래밍 연습문제
+ */
+
+namespace Test._06
+{
+    internal class _6_09
+    { }
+}
---
+++ Test/06/6_10.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 날짜: 2022/06/24
+ * 이름: 조귀애
+ * 내용: 예외처리 연습문제
+ */
+
+namespace Test._06
+{
+    internal class _6_10
+    {
+    }
+}
