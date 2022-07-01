b333d43948250650a5fa24e13d7d4230d67789c0
JOKWIAE <gkdiqkf@naver.com>
2022-07-01 14:54:06 +0900

    2022-06-15 교재예제

---
+++ Book/CH07/P211.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.CH07
+{
+    internal class P211
+    {
+        static void Main1(string[] args)
+        {
+            //코드5-4
+            int[] intArray = new int[10];
+            long[] longArray = new long[10];
+            string[] stringArray = new string[10];
+
+            //코드5-5
+            List<int> list = new List<int>();
+        }
+    }
+}
---
+++ Book/CH07/P213.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book.CH07
+{
+    internal class P213
+    {
+        static void Main1(string[] args)
+        {
+            //코드5-6
+            List<int> list = new List<int>();
+
+            list.Add(52);
+            list.Add(273);
+            list.Add(32);
+            list.Add(64);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("Count: " + list.Count + "\titem: " + item);
+            }
+
+            //코드5-7
+            List<int> list2 = new List<int>() { 52, 273, 32, 64 };
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("Count: " + list2.Count + "\titem: " + item);
+            }
+
+        }
+    }
+}
